1b438755eea22e380d5a3063d402d304acda4e22
Milos Popovic <milos_popovic@outlook.com>
2020-09-23 02:19:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,18 +8,18 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
-   
+
     @analyzed_text = TextAnalyzer.new(text_from_user)
-   
+
     erb :results
   end
 end
 #it starts by user going to a home page "/" which
 #transfers us to erb :index
-#when in index, we have a form, that says 
+#when in index, we have a form, that says
 #method POST action "/" which means
 #go to POST on a home page
-#back in the controller we access that where we 
+#back in the controller we access that where we
 #save the user's text(params) in a variable text_from_user
 #which is then used as an attribute for TextAnalyzer.new
 #which we defined in models
